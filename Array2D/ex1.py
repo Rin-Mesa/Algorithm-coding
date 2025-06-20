8bfeaf08cbea37022c78938503dde0548d93acd6
$ git config --global user.name ^[[200~mesa.rin@student.passerellesnumeriques.or <mesa.rin@student.passerellesnumeriques.org>
2025-06-20 13:01:44 +0700

    code

--- Array2D/ex1.py
+++ Array2D/ex1.py
@@ -14,20 +14,20 @@
 
 
 
-# array2D = [ [2, 3, 9, 0, 7],
-#             [8, 4, 6, 2, 1],
-#             [1, 2, 1, 1, 9],
-#             [4, 1, 5, 8, 6],
-#             [9, 5, 3, 9, 6]
-#             ]
-# for row in range(len(array2D)):
-#     for col in range(len(array2D[row])):
-#         if row + col == len(array2D)-1:
-#             value = "*"
-#         else:
-#             value = array2D[row][col]
-#         print(value, end=" " )
-#     print()
+array2D = [ [2, 3, 9, 0, 7],
+            [8, 4, 6, 2, 1],
+            [1, 2, 1, 1, 9],
+            [4, 1, 5, 8, 6],
+            [9, 5, 3, 9, 6]
+            ]
+for row in range(len(array2D)):
+    for col in range(len(array2D[row])):
+        if row + col == len(array2D)-1:
+            value = "*"
+        else:
+            value = array2D[row][col]
+        print(value, end=" " )
+    print()
 
 # array2D = [ [2, 3, 9, 0, 7],
 #             [8, 4, 6, 2, 1],
@@ -47,20 +47,20 @@
 
 
 
-array2D = [ [2, 3, 9, 0, 7],
-            [8, 4, 6, 2, 1],
-            [1, 2, 1, 1, 9],
-            [4, 1, 5, 8, 6],
-            [9, 5, 3, 9, 6]
-            ]
-for row in range(len(array2D)):
-    for col in range(len(array2D[row])):
-        if col == row or row + col ==len(array2D) -1:
-            value = "*"
-        else:
-            value = array2D[row][col]
-        print(value, end=" " )
-    print()
+# array2D = [ [2, 3, 9, 0, 7],
+#             [8, 4, 6, 2, 1],
+#             [1, 2, 1, 1, 9],
+#             [4, 1, 5, 8, 6],
+#             [9, 5, 3, 9, 6]
+#             ]
+# for row in range(len(array2D)):
+#     for col in range(len(array2D[row])):
+#         if col == row or row + col ==len(array2D) -1:
+#             value = "*"
+#         else:
+#             value = array2D[row][col]
+#         print(value, end=" " )
+#     print()
 
 
 
---
+++ Sudstring/Ex1.py
@@ -0,0 +1,12 @@
+# name = "Mesa"
+# print(name[0:2])
+
+name = 'class26B'
+# print(name[2:])
+# print(name[2:8])
+
+# print(name[0:7])
+# print(name[:7])
+# print(name[:-1])
+
+print(name[0:8:2])
